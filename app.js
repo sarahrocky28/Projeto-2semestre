045543fd51c6eec112ebf80aad0f9ee88f3e839d
sarahrocky28 <sarah109923753@gmail.com>
2026-08-14 20:29:17 -0300

    fiz 3 funções usando os dados de contatos.js

---
+++ app.js
@@ -0,0 +1,18 @@
+//Para listar os usuarios
+import { listarUsuarios } from "./contatos.js";
+
+(listarUsuarios());
+
+//Paralitar contatos do usuarios
+import { idUser } from "./contatos.js"
+
+(idUser(1));
+
+
+//listar mensagens dos usuarios
+import { ContactMsg, } from "./contatos.js";
+
+(ContactMsg(1, 0));
+
+
+
---
+++ contatos.js
@@ -0,0 +1,618 @@
+export const usuarios = {
+  "whats-users": [
+    {
+      id: 1,
+      account: "Ricardo da Silva",
+      nickname: "Ricky",
+      "created-since": {
+        start: "2015-08-23",
+        end: "null",
+      },
+      gender: "male",
+      "profile-image": "",
+      number: "11987876567",
+      background: "#fccc5b",
+      contacts: [
+        {
+          name: "Ana Maria",
+          number: "26999999963",
+          description: "Frontend Developer",
+          image: "26999999963.png",
+          gender: "female",
+          messages: [
+            {
+              sender: "me",
+              content: "Olá Ana, como você está hoje?",
+              time: "14:20",
+            },
+            {
+              sender: "Ana Maria",
+              content: "Oi, estou bem. Obrigado por perguntar.",
+              time: "14:25",
+            },
+            {
+              sender: "me",
+              content: "Que bom ouvir isso. Você tem algum plano para o fim de semana?",
+              time: "14:30",
+            },
+            {
+              sender: "Ana Maria",
+              content: "Ainda não. Estou pensando em ir à praia. E você?",
+              time: "14:35",
+            },
+          ],
+        },
+        {
+          name: "Jane Smith",
+          number: "269999799601",
+          description: "UI Designer",
+          image: "269999799601.png",
+          gender: "female",
+          messages: [
+            {
+              sender: "me",
+              content: "Oi Jane, como vão as coisas?",
+              time: "09:00",
+            },
+            {
+              sender: "Jane Smith",
+              content: "Estou bem, obrigada. E você?",
+              time: "09:05",
+            },
+            {
+              sender: "me",
+              content: "Estou indo bem. A propósito, você viu os últimos designs para o novo projeto?",
+              time: "09:10",
+            },
+            {
+              sender: "Jane Smith",
+              content: "Sim, eu vi. Ficaram ótimos!",
+              time: "09:15",
+            },
+          ],
+        },
+        {
+          name: "Mark Johnson",
+          number: "26999999964",
+          description: "Backend Developer",
+          image: "26999999964.png",
+          gender: "male",
+          messages: [
+            {
+              sender: "me",
+              content: "Oi Mark, como estão as coisas?",
+              time: "13:00",
+            },
+            {
+              sender: "Mark Johnson",
+              content: "Tudo indo bem. E com você?",
+              time: "13:05",
+            },
+            {
+              sender: "me",
+              content: "Estou bem. A propósito, você tem alguma atualização sobre o desenvolvimento da API?",
+              time: "13:10",
+            },
+            {
+              sender: "Mark Johnson",
+              content: "Sim, estamos fazendo um bom progresso. Devemos conseguir entregar dentro do prazo.",
+              time: "13:15",
+            },
+          ],
+        },
+        {
+          name: "Sarah Lee",
+          number: "26999999965",
+          description: "Software Engineer",
+          image: "26999999965.png",
+          gender: "female",
+          messages: [
+            {
+              sender: "me",
+              content: "Hey Sarah, como estão as coisas?",
+              time: "10:00",
+            },
+            {
+              sender: "Sarah Lee",
+              content: "Ei! Tudo bem por aqui e contigo?",
+              time: "10:05",
+            },
+            {
+              sender: "me",
+              content: "Estou bem também! O que tem feito ultimamente?",
+              time: "10:10",
+            },
+            {
+              sender: "Sarah Lee",
+              content: "Tenho trabalhado bastante, mas está tudo bem. E você?",
+              time: "10:15",
+            },
+          ],
+        },
+        {
+          name: "John Doe",
+          number: "26999999967",
+          description: "Designer",
+          image: "26999999967.png",
+          gender: "male",
+          messages: [
+            {
+              sender: "John Doe",
+              content: "Oi, tudo bem?",
+              time: "11:00",
+            },
+            {
+              sender: "me",
+              content: "Tudo ótimo! E contigo?",
+              time: "11:05",
+            },
+            {
+              sender: "John Doe",
+              content: "Estou bem também! Viu aquela nova ferramenta de design?",
+              time: "11:10",
+            },
+            {
+              sender: "John Doe",
+              content: "Acho que você vai gostar!",
+              time: "11:10",
+            },
+          ],
+        },
+        {
+          name: "Julia Smith",
+          number: "26999999968",
+          description: "Product Manager",
+          image: "26999999968.png",
+          gender: "female",
+          messages: [
+            {
+              sender: "Julia Smith",
+              content: "Oi, como está indo o projeto?",
+              time: "12:00",
+            },
+            {
+              sender: "me",
+              content: "Está indo bem, estamos quase terminando a fase de testes!",
+              time: "12:05",
+            },
+            {
+              sender: "Julia Smith",
+              content: "Ótimo! E como está o time?",
+              time: "12:10",
+            },
+            {
+              sender: "me",
+              content: "Todos estão trabalhando duro e focados no objetivo!",
+              time: "12:15",
+            },
+          ],
+        },
+        {
+          name: "Max Kellerman",
+          number: "26999999969",
+          description: "Marketing Manager",
+          image: "26999999969.png",
+          gender: "male",
+          messages: [
+            {
+              sender: "Max Kellerman",
+              content: "Ei! Vi que vocês estão com uma nova campanha no ar!",
+              time: "13:00",
+            },
+            {
+              sender: "me",
+              content: "Sim, estamos testando algumas coisas novas!",
+              time: "13:05",
+            },
+            {
+              sender: "Max Kellerman",
+              content: "Bom trabalho, fico feliz em ver vocês tentando coisas novas!",
+              time: "13:10",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 2,
+      account: "Bernardo Xavier Ribeiro",
+      nickname: "BeeR",
+      "created-since": {
+        start: "2020-02-13",
+        end: "null",
+      },
+      gender: "male",
+      "profile-image": "",
+      number: "11966578996",
+      background: "#fccc5b",
+      contacts: [
+        {
+          name: "José Maria da Silva",
+          number: "26999999910",
+          description: "Developer",
+          image: "26999999910.png",
+          gender: "male",
+          messages: [
+            {
+              sender: "me",
+              content: "Olá papai, como você está hoje?",
+              time: "14:20",
+            },
+            {
+              sender: "José Maria da Silva",
+              content: "Oi, estou bem. Obrigado por perguntar.",
+              time: "14:25",
+            },
+            {
+              sender: "me",
+              content: "Que bom ouvir isso. Você tem algum plano para o fim de semana?",
+              time: "14:30",
+            },
+            {
+              sender: "José Maria da Silva",
+              content: "Ainda não. Estou pensando em ir à praia. E você?",
+              time: "14:35",
+            },
+          ],
+        },
+        {
+          name: "John Guttemberg",
+          number: "26999999911",
+          description: "Free",
+          image: "26999999110.png",
+          gender: "male",
+          messages: [
+            {
+              sender: "me",
+              content: "Oi John, como vão as coisas?",
+              time: "09:00",
+            },
+            {
+              sender: "John Guttemberg",
+              content: "Estou bem, obrigado. E você?",
+              time: "09:05",
+            },
+            {
+              sender: "me",
+              content: "Estou indo bem. A propósito, você viu os últimos designs para o novo projeto?",
+              time: "09:10",
+            },
+            {
+              sender: "John Guttemberg",
+              content: "Sim, eu vi. Ficaram ótimos!",
+              time: "09:15",
+            },
+          ],
+        },
+        {
+          name: "George Mikael",
+          number: "26999999912",
+          description: "Backend Developer",
+          image: "26999999912.png",
+          gender: "male",
+          messages: [
+            {
+              sender: "me",
+              content: "Oi Mark, como estão as coisas?",
+              time: "13:00",
+            },
+            {
+              sender: "George Mikael",
+              content: "Tudo indo bem. E com você?",
+              time: "13:05",
+            },
+            {
+              sender: "me",
+              content: "Estou bem. A propósito, você tem alguma atualização sobre o desenvolvimento da API?",
+              time: "13:10",
+            },
+            {
+              sender: "George Mikael",
+              content: "Sim, estamos fazendo um bom progresso. Devemos conseguir entregar dentro do prazo.",
+              time: "13:15",
+            },
+          ],
+        },
+        {
+          name: "Cristian Lee",
+          number: "26999999913",
+          description: "Software Engineer",
+          image: "26999999912.png",
+          gender: "male",
+          messages: [
+            {
+              sender: "me",
+              content: "Hey Sarah, como estão as coisas?",
+              time: "10:00",
+            },
+            {
+              sender: "Cristian Lee",
+              content: "Ei! Tudo bem por aqui e contigo?",
+              time: "10:05",
+            },
+            {
+              sender: "me",
+              content: "Estou bem também! O que tem feito ultimamente?",
+              time: "10:10",
+            },
+            {
+              sender: "Cristian Lee",
+              content: "Tenho trabalhado bastante, mas está tudo bem. E você?",
+              time: "10:15",
+            },
+          ],
+        },
+        {
+          name: "Jonny Devited",
+          number: "26999999914",
+          description: "Designer",
+          image: "26999999914.png",
+          gender: "male",
+          messages: [
+            {
+              sender: "Jonny Devited",
+              content: "Oi, tudo bem?",
+              time: "11:00",
+            },
+            {
+              sender: "me",
+              content: "Tudo ótimo! E contigo?",
+              time: "11:05",
+            },
+            {
+              sender: "Jonny Devited",
+              content: "Estou bem também! Viu aquela nova ferramenta de design?",
+              time: "11:10",
+            },
+            {
+              sender: "Jonny Devited",
+              content: "Acho que você vai gostar!",
+              time: "11:10",
+            },
+          ],
+        },
+        {
+          name: "Shita Ling",
+          number: "26999999916",
+          description: "Product Manager",
+          image: "26999999916.png",
+          gender: "male",
+          messages: [
+            {
+              sender: "Shita Ling",
+              content: "Oi, como está indo o projeto?",
+              time: "12:00",
+            },
+            {
+              sender: "me",
+              content: "Está indo bem, estamos quase terminando a fase de testes!",
+              time: "12:05",
+            },
+            {
+              sender: "Shita Ling",
+              content: "Ótimo! E como está o time?",
+              time: "12:10",
+            },
+            {
+              sender: "me",
+              content: "Todos estão trabalhando duro e focados no objetivo!",
+              time: "12:15",
+            },
+          ],
+        },
+        {
+          name: "Nilsen Silva",
+          number: "26999999917",
+          description: "Marketing Manager",
+          image: "26999999917.png",
+          gender: "male",
+          messages: [
+            {
+              sender: "Nilsen Silva",
+              content: "Ei! Vi que vocês estão com uma nova campanha no ar!",
+              time: "13:00",
+            },
+            {
+              sender: "me",
+              content: "Sim, estamos testando algumas coisas novas!",
+              time: "13:05",
+            },
+            {
+              sender: "Nilsen Silva",
+              content: "Bom trabalho, fico feliz em ver vocês tentando coisas novas!",
+              time: "13:10",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 3,
+      account: "Sandy Wilstern",
+      nickname: "Sand",
+      "created-since": {
+        start: "2017-05-11",
+        end: "null",
+      },
+      gender: "female",
+      "profile-image": "",
+      number: "11955577796",
+      background: "#cc5bfc",
+      contacts: [
+        {
+          name: "Peter Wilsen",
+          number: "26999999920",
+          description: "Engineer",
+          image: "26999999920.png",
+          gender: "male",
+          messages: [
+            {
+              sender: "me",
+              content: "Hey Sarah, como estão as coisas?",
+              time: "10:00",
+            },
+            {
+              sender: "Peter Wilsen",
+              content: "Ei! Tudo bem por aqui e contigo?",
+              time: "10:05",
+            },
+            {
+              sender: "me",
+              content: "Estou bem também! O que tem feito ultimamente?",
+              time: "10:10",
+            },
+            {
+              sender: "Peter Wilsen",
+              content: "Tenho trabalhado bastante, mas está tudo bem. E você?",
+              time: "10:15",
+            },
+          ],
+        },
+        {
+          name: "Jonny Devited",
+          number: "26999999921",
+          description: "Designer",
+          image: "26999999921.png",
+          gender: "male",
+          messages: [
+            {
+              sender: "Jonny Devited",
+              content: "Oi, tudo bem?",
+              time: "11:00",
+            },
+            {
+              sender: "me",
+              content: "Tudo ótimo! E contigo?",
+              time: "11:05",
+            },
+            {
+              sender: "Jonny Devited",
+              content: "Estou bem também! Viu aquela nova ferramenta de design?",
+              time: "11:10",
+            },
+            {
+              sender: "Jonny Devited",
+              content: "Acho que você vai gostar!",
+              time: "11:10",
+            },
+          ],
+        },
+        {
+          name: "Franklin Silva",
+          number: "26999999922",
+          description: "Product Manager",
+          image: "26999999922.png",
+          gender: "male",
+          messages: [
+            {
+              sender: "Franklin Silva",
+              content: "Oi, como está indo o projeto?",
+              time: "12:00",
+            },
+            {
+              sender: "me",
+              content: "Está indo bem, estamos quase terminando a fase de testes!",
+              time: "12:05",
+            },
+            {
+              sender: "Franklin Silva",
+              content: "Ótimo! E como está o time?",
+              time: "12:10",
+            },
+            {
+              sender: "me",
+              content: "Todos estão trabalhando duro e focados no objetivo!",
+              time: "12:15",
+            },
+          ],
+        },
+        {
+          name: "Catarina Sousa",
+          number: "26999999923",
+          description: "Marketing Manager",
+          image: "26999999923.png",
+          gender: "female",
+          messages: [
+            {
+              sender: "Catarina Sousa",
+              content: "Ei! Vi que vocês estão com uma nova campanha no ar!",
+              time: "13:00",
+            },
+            {
+              sender: "me",
+              content: "Sim, estamos testando algumas coisas novas!",
+              time: "13:05",
+            },
+            {
+              sender: "Catarina Sousa",
+              content: "Bom trabalho, fico feliz em ver vocês tentando coisas novas!",
+              time: "13:10",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 4,
+      account: "Jonathan Xavier",
+      nickname: "Joe",
+      "created-since": {
+        start: "2023-03-11",
+        end: "null",
+      },
+      gender: "male",
+      "profile-image": "",
+      number: "1194457796",
+      background: "#c00bfc",
+      contacts: [
+        {
+          name: "Wally Gator",
+          number: "26999999930",
+          description: "Free",
+          image: "26999999930.png",
+          gender: "male",
+          messages: [
+            {
+              sender: "me",
+              content: "Hey Sarah, como estão as coisas?",
+              time: "10:00",
+            },
+            {
+              sender: "Wally Gator",
+              content: "Ei! Tudo bem por aqui e contigo?",
+              time: "10:05",
+            },
+            {
+              sender: "me",
+              content: "Estou bem também! O que tem feito ultimamente?",
+              time: "10:10",
+            },
+            {
+              sender: "Wally Gator",
+              content: "Tenho trabalhado bastante, mas está tudo bem. E você?",
+              time: "10:15",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+//Função para listar usuarios
+
+export function listarUsuarios() {
+  console.table(usuarios["whats-users"]);
+};
+
+
+
+export function idUser(user) {
+  console.table(usuarios["whats-users"][user].contacts);
+};
+
+
+export function ContactMsg(user, contact) {
+  console.table(usuarios["whats-users"][user].contacts[contact].messages);
+};
+
