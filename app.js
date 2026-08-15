ae6b3cf0f62621ed1f9442818ef7e048e02064fa
sarahrocky28 <sarah109923753@gmail.com>
2026-08-14 22:09:28 -0300

    desenvolvendo funções para adicionar contatos no layout

--- app.js
+++ app.js
@@ -16,3 +16,20 @@ import { ContactMsg, } from "./contatos.js";
 
 
 
+const elemento = {
+
+}
+
+function criarContatos(srcFoto, nome, hora, ultima, naoLidas, idContato)
+
+const cardContainer = document.createElement("article");
+const fotoContato = document.createElement("img");
+const nomeContato = document.createElement("p");
+const hora_msg_recebida = document.createElement("p");
+const msg_recebida = document.createElement("p");
+const msg_nao_lidas = document.createElement("p");
+
+cardContainer.className = "container_msg";
+fotoContato.className = "foto";
+nomeContato.className = "nome";
+hora_msg_recebida.className = "nome";
