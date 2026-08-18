edd233e2c4bfd9587e73354edfa2f1d1a7d207d7
sarahrocky28 <sarah109923753@gmail.com>
2026-08-17 21:40:34 -0300

    caminhando para injetar as conversas no layout

--- app.js
+++ app.js
@@ -29,7 +29,46 @@ const hora_msg_recebida = document.createElement("p");
 const msg_recebida = document.createElement("p");
 const msg_nao_lidas = document.createElement("p");
 
+
+//mudar no css
 cardContainer.className = "container_msg";
 fotoContato.className = "foto";
 nomeContato.className = "nome";
-hora_msg_recebida.className = "nome";
+hora_msg_recebida.className = "hora_msg_recebida";
+msg_recebida.className = "ultimaMsg"
+msg_nao_lidas.className = "msg_nao_lidas"
+
+fotoContato.src = `https://i.pravatar.cc/207?img=${idContato + 1}`;
+nomeContato.innerText = nome;
+hora_msg_recebida.innerText = hora;
+msg_recebida.innerText = ultima;
+msg_nao_lidas.innerText = naoLidas;
+
+
+cardContainer.id = idContato;
+
+
+const listarUsuarios =
+
+
+
+
+
+
+
+
+
+    fotoPerfil.addEventListener("click", function () {
+        const telaIncial = fotoPerfil.classList.contains("ativo");
+        if (telaIncial) {
+            Perfil.classList.remove("ativo");
+            container.innerHTML = contatos;
+        } else {
+            Perfil.classList.add("ativo");
+
+            container.innerHTML = 'Abluba'
+        }
+    });
+
+
+
